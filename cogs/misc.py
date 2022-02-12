8f2742f5e1e8989175ee69d663a84b0cc8c8c2a0
Dylan J <dylanjames420@protonmail.com>
2022-02-11 21:37:47 -0500

    Create misc.py

---
+++ cogs/misc.py
@@ -0,0 +1,12 @@
+# **Misc Commands**
+This command category holds a handful of bot commands and functions that do not fall into any other cogs.
+
+|   Command | Description   | Usage   | Notes | Command Aliases
+| ------------ | ------------ | ------------ | ------------ |  ------------ | 
+|.ping   | Returns the bot's API and message latency. |  .ping | N/A |  N/A
+|.dm | DM's a specific user a provided message. | .dm [user] [message] | Global admin is needed to run this command. | N/A
+|.help  | Returns a link to the documentation. | .help | N/A | N/A
+| .addadmin | Add a user to the bot's database as a global admin. | .addadmin [user] | Global admin is needed to run this command. | N/A
+| .removeadmin | Remove a user from the bot's database as a global admin. | .addadmin [user] | Global admin is needed to run this command. | N/A
+| .afk | Set yourself as AFK with a reason which will be shown to other users if they ping you. | .afk [reason] | N/A | N/A
+ 
